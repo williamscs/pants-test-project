10553b891aa3d9424ccffe2ef7f75cb726bbb40b
Chris Williams <me@williamschris.com>
2021-09-23 16:34:12 -0500

    repro source not being pulled in

--- helloworld/python_google_cloud_function.py
+++ helloworld/python_google_cloud_function.py
@@ -3,9 +3,10 @@
 
 from helloworld.config import load_config
 from helloworld.greet.greeting import Greeter
-
+from helloworld.random import SomeClass
 
 def handler(event, context):
     config = load_config()
     greeter = Greeter(languages=config.languages, greetings=config.greetings)
+    SomeClass()
     print(greeter.greet("world"))
